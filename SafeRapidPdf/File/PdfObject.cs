f909793e457f3a7131a1907640078cf4e454aae2
Jaap de Haan <jaap.dehaan@color-of-code.de>
2012-02-17 00:56:14 +0100

    Formatting

--- SafeRapidPdf/File/PdfObject.cs
+++ SafeRapidPdf/File/PdfObject.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 namespace SafeRapidPdf.File
 {
-	public abstract class PdfObject : IPdfObject
+	public abstract class PdfObject: IPdfObject
 	{
 		protected PdfObject(PdfObjectType type)
 		{
@@ -15,7 +15,8 @@ namespace SafeRapidPdf.File
 
 		public PdfObjectType ObjectType
 		{
-			get; private set;
+			get;
+			private set;
 		}
 
 		public static PdfObject ParseAny(Lexical.ILexer lexer)
@@ -26,86 +27,93 @@ namespace SafeRapidPdf.File
 		public static PdfObject ParseAny(Lexical.ILexer lexer, String endToken)
 		{
 			String token = lexer.PeekToken();
-			if (token == null) return null;
+			if (token == null)
+				return null;
 
 			File.PdfObject obj = null;
 			switch (token)
 			{
-				// null object
-				case "null":
-					obj = PdfNull.Parse(lexer);
-					break;
-				case "%":
-					obj = PdfComment.Parse(lexer);
-					break;
-				case "true":
-				case "false":
-					obj = PdfBoolean.Parse(lexer);
-					break;
-				case "obj":
-					obj = PdfIndirectObject.Parse(lexer);
-					break;
-				case "R":
-					PdfIndirectReference ir = PdfIndirectReference.Parse(lexer);
-					ir.Resolver = lexer.IndirectReferenceResolver;
-					obj = ir;
-					break;
-				case "<<":
-					obj = PdfDictionary.Parse(lexer);
-					// check for stream and combine put dictionary into stream object
-					token = lexer.PeekToken();
-					if (token == "stream")
-						obj = PdfStream.Parse(obj as PdfDictionary, lexer);
-					break;
-				case "[":
-					obj = PdfArray.Parse(lexer);
-					break;
-				case "<":
-					obj = PdfHexadecimalString.Parse(lexer);
-					break;
-				case "(":
-					obj = PdfLiteralString.Parse(lexer);
-					break;
-				case "/":
-					obj = PdfName.Parse(lexer);
-					break;
-				case "xref":
-					obj = PdfXRef.Parse(lexer);
-					break;
-				case "trailer":
-					obj = PdfTrailer.Parse(lexer);
-					break;
+			// null object
+			case "null":
+				obj = PdfNull.Parse(lexer);
+				break;
+			case "%":
+				obj = PdfComment.Parse(lexer);
+				break;
+			case "true":
+			case "false":
+				obj = PdfBoolean.Parse(lexer);
+				break;
+			case "<<":
+				obj = PdfDictionary.Parse(lexer);
+				// check for stream and combine put dictionary into stream object
+				token = lexer.PeekToken();
+				if (token == "stream")
+					obj = PdfStream.Parse(obj as PdfDictionary, lexer);
+				break;
+			case "[":
+				obj = PdfArray.Parse(lexer);
+				break;
+			case "<":
+				obj = PdfHexadecimalString.Parse(lexer);
+				break;
+			case "(":
+				obj = PdfLiteralString.Parse(lexer);
+				break;
+			case "/":
+				obj = PdfName.Parse(lexer);
+				break;
+			case "xref":
+				obj = PdfXRef.Parse(lexer);
+				break;
+			case "trailer":
+				obj = PdfTrailer.Parse(lexer);
+				break;
 
-				case ")":
-				case ">":
-				case ">>":
-				case "]":
-				case "}":
-				case "stream":
-				case "endstream":
-				case "endobj":
-					if (endToken == token)
-					{
-						lexer.Expects(token);
-						return null; // expected end
-					}
-					throw new Exception("Parser error: out of sync");
+			case ")":
+			case ">":
+			case ">>":
+			case "]":
+			case "}":
+			case "stream":
+			case "endstream":
+			case "endobj":
+				if (endToken == token)
+				{
+					lexer.Expects(token);
+					return null; // expected end
+				}
+				throw new Exception("Parser error: out of sync");
 
-				case "startxref":
-					obj = PdfStartXRef.Parse(lexer);
-					break;
+			case "startxref":
+				obj = PdfStartXRef.Parse(lexer);
+				break;
 
-				default:
-					// must be an integer or double value
-					obj = PdfNumeric.Parse(lexer);
-					break;
+			case "obj":
+				obj = PdfIndirectObject.Parse(lexer);
+				break;
+
+			case "R":
+				PdfIndirectReference ir = PdfIndirectReference.Parse(lexer);
+				ir.Resolver = lexer.IndirectReferenceResolver;
+				obj = ir;
+				break;
+
+			default:
+				// must be an integer or double value
+				obj = PdfNumeric.Parse(lexer);
+				break;
 			}
 			if (obj == null)
 				throw new Exception("Parse error, could not read object");
 			return obj;
 		}
 
-		public bool IsContainer { get; protected set; }
+		public bool IsContainer
+		{
+			get;
+			protected set;
+		}
 
 		public String Text
 		{
@@ -117,7 +125,7 @@ namespace SafeRapidPdf.File
 
 		public virtual ReadOnlyCollection<IPdfObject> Items
 		{
-			get 
+			get
 			{
 				if (!IsContainer)
 					return null;
