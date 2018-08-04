f18efd0b1b20323af56d6e03b80f67daaf6bb1ee
Jaap de Haan <ColorOfCode@googlemail.com>
2018-08-04 14:40:48 +0200

    Added PdfInfoTool to test the library in batch scripts and bigger pdfs

---
+++ PdfInfoTool/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using SafeRapidPdf.File;
+
+namespace PdfInfoTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                var objects = PdfFile.Parse(args[0]);
+            }
+            else
+                Console.WriteLine("Usage: PdfInfoTool <path to pdf>");
+        }
+    }
+}
