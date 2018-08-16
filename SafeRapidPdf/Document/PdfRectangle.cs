f0c392abb69a46d4651c66ce7478e866e7f11365
Jason Nelson <jason@carbonmade.com>
2018-08-16 12:50:07 -0700

    Change PdfRectangle values from decimal to double

--- SafeRapidPdf/Document/PdfRectangle.cs
+++ SafeRapidPdf/Document/PdfRectangle.cs
@@ -14,19 +14,19 @@ namespace SafeRapidPdf.Document
                 throw new Exception("A rectangle must have 4 values!");
             }
 
-            Llx = ((PdfNumeric)box.Items[0]).ToDecimal();
-            Lly = ((PdfNumeric)box.Items[1]).ToDecimal();
-            Urx = ((PdfNumeric)box.Items[2]).ToDecimal();
-            Ury = ((PdfNumeric)box.Items[3]).ToDecimal();
+            Llx = (PdfNumeric)box.Items[0];
+            Lly = (PdfNumeric)box.Items[1];
+            Urx = (PdfNumeric)box.Items[2];
+            Ury = (PdfNumeric)box.Items[3];
         }
 
-        public decimal Llx { get; } // lower left x
+        public double Llx { get; } // lower left x
 
-        public decimal Lly { get; } // lower left y
+        public double Lly { get; } // lower left y
 
-        public decimal Urx { get; } // upper right x
+        public double Urx { get; } // upper right x
 
-        public decimal Ury { get; } // upper right y
+        public double Ury { get; } // upper right y
 
         public override string ToString() => $"{ObjectType} [{Llx}; {Lly}; {Urx}; {Ury}]";
     }
